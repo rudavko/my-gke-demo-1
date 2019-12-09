749845fbc37fc92cac12e09b7b7cbf5edafbdb06
Andrii <Rudavko@gmail.com>
2019-12-09 10:48:14 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 
 const handler = (request,response)=>{
-  response.write('Hey, whats up?')
+  response.write('Hey, whats up?'+process.env.TEST_SECRET)
   response.end()
 }
 
