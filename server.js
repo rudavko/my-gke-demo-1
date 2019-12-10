92a38614c4906281dc004d89fdcbc1ca7edd0525
Andrii <Rudavko@gmail.com>
2019-12-10 11:09:12 +0200

    Lint the server.js

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const http = require('http')
 
-const handler = (request,response)=>{
-  if(request.headers['x-forwarded-proto']==='http'){
+const handler = (request, response) => {
+  if (request.headers['x-forwarded-proto'] === 'http') {
     const url = [
       'https://',
       request.headers.host,
       request.url
     ]
-    
+
     response.writeHead(302, {
-    'Location': url.join('')
+      Location: url.join('')
     })
     response.end()
     return
@@ -17,13 +17,13 @@ const handler = (request,response)=>{
   const result = [
     'Hey, whats up?',
     process.env.TEST_SECRET,
-    JSON.stringify(Object.keys(request),null,4),
-    JSON.stringify(request.headers,null,4),
-    JSON.stringify(request.url,null,4)
+    JSON.stringify(Object.keys(request), null, 4),
+    JSON.stringify(request.headers, null, 4),
+    JSON.stringify(request.url, null, 4)
   ]
   response.write(result.join(' '))
   response.end()
 }
 
 http.createServer(handler)
-.listen(80)
+  .listen(80)
